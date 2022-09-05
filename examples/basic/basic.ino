65bc497f88f0e795adb583aac28ca9527df6e39c
Oleksandr Bonar <oleksandr.bonar@outlook.com>
2022-09-05 15:32:50 +0300

    Update basic.ino

--- examples/basic/basic.ino
+++ examples/basic/basic.ino
@@ -6,8 +6,8 @@ tle5010_spi sensor(PIN_CS);
 
 void setup() {
   // 4 MHZ square wave on pin 9
-  TCCR1A = (0<<COM1A1) | (1 << COM1A0) | (0 << COM1B1) | (0 << COM1B0) | (0 << WGM11) | (0 << WGM10);
-  TCCR1B = (0<<ICNC1) | (0 << ICES1) | (0 << WGM13) | (1 << WGM12) | (0 << CS12) | (0 << CS11) | (1 << CS10);
+  TCCR1A = (0 << COM1A1) | (1 << COM1A0) | (0 << COM1B1) | (0 << COM1B0) | (0 << WGM11) | (0 << WGM10);
+  TCCR1B = (0 << ICNC1) | (0 << ICES1) | (0 << WGM13) | (1 << WGM12) | (0 << CS12) | (0 << CS11) | (1 << CS10);
   OCR1A = 0x01;
   ICR1 = 0;
   pinMode(9, OUTPUT);
