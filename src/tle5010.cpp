9bec24b0d51ba1a178a8edbed0ed52dd426e11bb
Oleksandr Bonar <oleksandr.bonar@outlook.com>
2022-09-05 14:58:24 +0300

    Create tle5010.cpp

---
+++ src/tle5010.cpp
@@ -0,0 +1,76 @@
+#include <tle5010.h>
+
+tle5010::tle5010()
+{
+  atan2FuncInt = _atan2IntDefault;
+}
+
+double tle5010::readAngleRadians()
+{
+  readXY();
+
+  return atan2Func(y, x);
+}
+
+double tle5010::readAngleDegrees()
+{
+  return readAngleRadians() * (180 / PI);
+}
+
+int16_t tle5010::readInteger()
+{
+  readXY();
+  
+  return atan2FuncInt(y, x);
+}
+
+tle5010_spi::tle5010_spi(uint8_t _cs)
+{
+  csBM = _cs;
+}
+
+void tle5010_spi::begin(atan2Function _atan2Func)
+{
+   atan2Func = _atan2Func;
+   setCS(csBM);   
+   SPI.begin();
+}
+
+void tle5010_spi::setCS(uint8_t _cs)
+{
+  // caching CS PORT/DDR for faster IO
+  csO = portOutputRegister(digitalPinToPort(_cs));
+  csBM = digitalPinToBitMask(_cs);
+  csIBM = ~csBM;
+
+   *csO |= csBM;  // CS high
+   *(portModeRegister(digitalPinToPort(_cs))) |= csBM;  // =pinMode(_cs, OUTPUT);
+}
+
+void tle5010_spi::readXY()
+{
+  SPI.beginTransaction(SPISettings(3000000, MSBFIRST, SPI_MODE1));	// speed
+
+  *csO &= csIBM;  // CS low
+
+  #ifndef __TLE5010_TEST_MODE
+    // command for reading 4 bytes X&Y
+    // 10001100 = 1(read) 0001(register #1) 100 (4 bytes)
+    SPI.transfer16(0x008c);
+  #else
+    // testmode: command for reading 4 reserved bytes (expected FF 00 00 00)
+    // 11000100 = 1(read) 1000(register #8) 100 (4 bytes)
+    SPI.transfer16(0x00c4);
+  #endif
+
+  // reading bytes
+
+  ((uint8_t*)&x)[0] = SPI.transfer(0xff);
+  ((uint8_t*)&x)[1] = SPI.transfer(0xff);
+  ((uint8_t*)&y)[0] = SPI.transfer(0xff);
+  ((uint8_t*)&y)[1] = SPI.transfer(0xff);
+
+  *csO |= csBM; // CS high
+  
+  SPI.endTransaction();
+}
